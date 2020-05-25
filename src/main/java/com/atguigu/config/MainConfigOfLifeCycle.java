b72bd867d27be058094d76e117eed9908e6a2842
yaoyexing <2530121801@qq.com>
2020-05-25 16:04:15 +0800

    看完 15、生命周期-BeanPostProcessor-后置处理器 视频后代码

--- src/main/java/com/atguigu/config/MainConfigOfLifeCycle.java
+++ src/main/java/com/atguigu/config/MainConfigOfLifeCycle.java
@@ -16,8 +16,10 @@ import com.atguigu.bean.Car;
  * 构造（对象创建）
  * 		单实例：在容器启动时创建对象；
  * 		多实例：在每次获取时创建对象；
+ * BeanPostProcessor.postProcessBeforeInitialization
  * 初始化：
  * 		对象创建完成，并赋值好，调用初始化方法
+ * BeanPostProcessor.postProcessAfterInitialization
  * 销毁：
  * 		单实例：容器关闭的时候进行销毁	
  * 		多实例：容器不会管理这个bean，容器不会调用销毁方法
@@ -28,6 +30,13 @@ import com.atguigu.bean.Car;
  * 3）可以使用JSR250:
  * 		@PostConstruct：在bean创建完成并且属性赋值完成，来执行初始化方法
  * 		@PreDestroy：在容器销毁bean之前通知我们进行清理操作
+ * 4）BeanPostProcessor【interface】：bean的后置处理器；
+ * 		在bean初始化前后进行一些处理工作；
+ * 		postProcessBeforeInitialization：在初始化之前工作
+ * 		postProcessAfterInitialization：在初始化之后工作
+ * 
+ * 
+ * 
  * 
  * 
  * @author yaoran
---
+++ src/main/java/com/atguigu/config/MyBeanPostProcessor.java
@@ -0,0 +1,28 @@
+package com.atguigu.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+/**
+ * 后置处理器，初始化前后进行工作
+ * 将后置处理器加入到容器中
+ * @author yaoran
+ *
+ */
+@Component
+public class MyBeanPostProcessor implements BeanPostProcessor{
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("postProcessBeforeInitialization..."+beanName+"=>"+bean);
+		return bean;
+	}
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("postProcessAfterInitialization..."+beanName+"=>"+bean);
+		return bean;
+	}
+
+}
